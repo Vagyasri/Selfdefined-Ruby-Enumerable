f1c5c7ac4fdb0feb399e8d1de6e4456f58133feb
Lucky <vagyasri2011@gmail.com>
2021-06-04 09:39:55 +0200

    Add my_each

--- main.rb
+++ main.rb
@@ -1,12 +1,23 @@
 module Enumerable
-    def my_each(array)
-        i = 0
-        while i < array.length
-            if block_given?
-            yield array[i]
-            i += 1
-            end
-        end
-        array   
-    end
+  def my_each
+    if block_given?
+      to_a.length.times { |i| yield to_a[i] }
+    else
+     to_enum  
+    end  
   end
+# def my_each
+#     return enum_for unless block_given?
+#     for index in self
+#       yield index
+#     end
+#   end
+end
+
+# my_hash = {min: 2, max: 5}
+# puts my_hash.my_each
+# my_hash = {min: 2, max: 5}
+# my_hash.my_each { |key, value| puts "k: #{key}, v: #{value}" }
+# my_list = [1, 2, 3, 4 ,5]
+# [3..-1].my_each { |i| puts i }
+# my_list[3..-1].my_each { |i| puts i }
